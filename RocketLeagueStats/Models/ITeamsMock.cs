d9dcb4f316936eef54f9366b0a723229bc978546
Dylan <dylansprague11@hotmail.com>
2018-11-24 01:28:39 -0500

    Added mock data and first unit test

---
+++ RocketLeagueStats/Models/ITeamsMock.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RocketLeagueStats.Models
+{
+    public interface ITeamsMock
+    {
+        IQueryable<team> teams { get; }
+
+        team Save(team team);
+        void Delete(team team);
+    }
+}
