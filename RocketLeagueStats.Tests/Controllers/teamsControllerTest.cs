ffc7088c5f97b4162d8f068f6785868c8d5341a4
Dylan <dylansprague11@hotmail.com>
2018-11-24 11:05:14 -0500

    Added moq data and first unit test

---
+++ RocketLeagueStats.Tests/Controllers/teamsControllerTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using RocketLeagueStats.Controllers;
+using RocketLeagueStats.Models;
+
+namespace RocketLeagueStats.Tests.Controllers
+{
+    [TestClass]
+    public class teamsControllerTest
+    {
+        Mock<ITeamsMock> mock;
+        List<team> teams;
+        teamsController controller;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            //arrange mock data for all unit tests
+            mock = new Mock<ITeamsMock>();
+
+            teams = new List<team>
+            {
+                new team { teamid=100, name="Team One", region="NA", wins=10, losses=4},
+                new team { teamid=200, name="Team Two", region="EU", wins=9, losses=5},
+                new team { teamid=300, name="Team Three", region="OCE", wins=6, losses=6}
+            };
+            //populate interface from mock data
+            mock.Setup(mock => mock.teams).Returns(teams.AsQueryable());
+            controller = new teamsController(mock.Object);
+        }
+
+        [TestMethod]
+        public void IndexReturnsView()
+        {
+            //act
+            ViewResult result = controller.Index() as ViewResult;
+
+            //assert
+            Assert.AreEqual("Index", result.ViewName);
+        }
+    }
+}
---
+++ RocketLeagueStats/Models/EFTeams.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RocketLeagueStats.Models
+{
+    public class EFTeams : ITeamsMock
+    {
+        //db connection moved here from teams controller
+        private RocketLeagueStatsModel db = new RocketLeagueStatsModel();
+        public IQueryable<team> teams { get { return db.teams; } }
+
+        public void Delete(team team)
+        {
+            db.teams.Remove(team);
+            db.SaveChanges();
+        }
+
+        public team Save(team team)
+        {
+            if (team.teamid == 0)
+            {
+                db.teams.Add(team);
+            }
+            else
+            {
+                db.Entry(team).State = System.Data.Entity.EntityState.Modified;
+            }
+            db.SaveChanges();
+            return team;
+        }
+    }
+}
